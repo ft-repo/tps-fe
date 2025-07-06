99d75e5c46eb65a188bf47b90704da5c95eb1afc
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-07-07 06:05:21 +0700

    fix: update navigation routes

--- src/configs/navigation-icon.config.tsx
+++ src/configs/navigation-icon.config.tsx
@@ -1,20 +1,31 @@
 import {
-    HiOutlineColorSwatch,
-    HiOutlineDesktopComputer,
-    HiOutlineTemplate,
-    HiOutlineViewGridAdd,
-    HiOutlineHome,
+	HiOutlineColorSwatch,
+	HiOutlineDesktopComputer,
+	HiOutlineTemplate,
+	HiOutlineViewGridAdd,
+	HiOutlineHome,
 } from 'react-icons/hi'
+import {
+	LuMapPin,
+	LuListChecks,
+	LuTruck,
+	LuServer,
+} from "react-icons/lu";
 import type { JSX } from 'react'
 
 export type NavigationIcons = Record<string, JSX.Element>
 
 const navigationIcon: NavigationIcons = {
-    home: <HiOutlineHome />,
-    singleMenu: <HiOutlineViewGridAdd />,
-    collapseMenu: <HiOutlineTemplate />,
-    groupSingleMenu: <HiOutlineDesktopComputer />,
-    groupCollapseMenu: <HiOutlineColorSwatch />,
+	home: <HiOutlineHome />,
+	singleMenu: <HiOutlineViewGridAdd />,
+	collapseMenu: <HiOutlineTemplate />,
+	groupSingleMenu: <HiOutlineDesktopComputer />,
+	groupCollapseMenu: <HiOutlineColorSwatch />,
+	// CUSTOM MENUBAR
+	routeEstimation: <LuMapPin />,
+	permitList: <LuListChecks />,
+	vehicleList: <LuTruck />,
+	executiveData: <LuServer />,
 }
 
 export default navigationIcon
--- src/configs/routes.config/routes.config.ts
+++ src/configs/routes.config/routes.config.ts
@@ -13,43 +13,67 @@ export const protectedRoutes = [
     },
     /** Example purpose only, please remove */
     {
-        key: 'singleMenuItem',
-        path: '/single-menu-view',
-        component: lazy(() => import('@/views/demo/SingleMenuView')),
+        key: 'routeEstimation',
+        path: '/route-estimation',
+        component: lazy(() => import('@/views/admin/route-estimation')),
         authority: [],
     },
     {
-        key: 'collapseMenu.item1',
-        path: '/collapse-menu-item-view-1',
-        component: lazy(() => import('@/views/demo/CollapseMenuItemView1')),
+        key: 'permitList',
+        path: '/permit-list',
+        component: lazy(() => import('@/views/admin/permit-list')),
         authority: [],
     },
     {
-        key: 'collapseMenu.item2',
-        path: '/collapse-menu-item-view-2',
-        component: lazy(() => import('@/views/demo/CollapseMenuItemView2')),
+        key: 'vehicleList',
+        path: '/vehicle-list',
+        component: lazy(() => import('@/views/admin/vehicle-list')),
         authority: [],
     },
     {
-        key: 'groupMenu.single',
-        path: '/group-single-menu-item-view',
-        component: lazy(() => import('@/views/demo/GroupSingleMenuItemView')),
+        key: 'executiveData',
+        path: '/executive-data',
+        component: lazy(() => import('@/views/admin/executive-data')),
         authority: [],
-    },
-    {
-        key: 'groupMenu.collapse.item1',
-        path: '/group-collapse-menu-item-view-1',
-        component: lazy(
-            () => import('@/views/demo/GroupCollapseMenuItemView1'),
-        ),
-        authority: [],
-    },
-    {
-        key: 'groupMenu.collapse.item2',
-        path: '/group-collapse-menu-item-view-2',
-        component: lazy(
-            () => import('@/views/demo/GroupCollapseMenuItemView2'),
-        ),
-        authority: [],
-    },
+    }
+    // {
+    //     key: 'singleMenuItem',
+    //     path: '/single-menu-view',
+    //     component: lazy(() => import('@/views/demo/SingleMenuView')),
+    //     authority: [],
+    // },
+    // {
+    //     key: 'collapseMenu.item1',
+    //     path: '/collapse-menu-item-view-1',
+    //     component: lazy(() => import('@/views/demo/CollapseMenuItemView1')),
+    //     authority: [],
+    // },
+    // {
+    //     key: 'collapseMenu.item2',
+    //     path: '/collapse-menu-item-view-2',
+    //     component: lazy(() => import('@/views/demo/CollapseMenuItemView2')),
+    //     authority: [],
+    // },
+    // {
+    //     key: 'groupMenu.single',
+    //     path: '/group-single-menu-item-view',
+    //     component: lazy(() => import('@/views/demo/GroupSingleMenuItemView')),
+    //     authority: [],
+    // },
+    // {
+    //     key: 'groupMenu.collapse.item1',
+    //     path: '/group-collapse-menu-item-view-1',
+    //     component: lazy(
+    //         () => import('@/views/demo/GroupCollapseMenuItemView1'),
+    //     ),
+    //     authority: [],
+    // },
+    // {
+    //     key: 'groupMenu.collapse.item2',
+    //     path: '/group-collapse-menu-item-view-2',
+    //     component: lazy(
+    //         () => import('@/views/demo/GroupCollapseMenuItemView2'),
+    //     ),
+    //     authority: [],
+    // },
 ]
