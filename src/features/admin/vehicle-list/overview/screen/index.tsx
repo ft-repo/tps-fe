2df32954116c15dbc5b57b465f8f69a36645e2ef
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-07-07 16:43:32 +0700

    feat: added vehicle-list components

---
+++ src/features/admin/vehicle-list/overview/screen/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const OverviewScreen: React.FC<Props> = (props) => {
+  const { } = props
+
+  return (
+    <div>index</div>
+  )
+}
+
+export default React.memo<Props>(OverviewScreen)
---
+++ src/views/admin/vehicle-list/create/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const CreateIndex: React.FC<Props> = (props) => {
+  const { } = props
+
+  return (
+    <div>index</div>
+  )
+}
+
+export default React.memo<Props>(CreateIndex)
