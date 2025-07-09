9359181c45d39054c4cae6065ea32cd59f43696f
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-07-09 18:26:43 +0700

    fix: removed unused route

--- src/features/admin/permit-list/overview/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FormSearchCategory } from './FormSearchCategory'
-export { default as FormSearchOther } from './FormSearchOther'
-export { default as TableCategory } from './TableCategory'
-export { default as TableOther } from './TableOther'
