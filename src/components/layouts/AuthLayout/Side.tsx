353037f81caf67bbdf803cb75befe532ffb16163
f1uke <26509248+lzy3me@users.noreply.github.com>
2025-07-30 05:13:59 +0700

    feat: enhance authentication forms with additional fields and localized text

--- src/components/layouts/AuthLayout/Side.tsx
+++ src/components/layouts/AuthLayout/Side.tsx
@@ -12,40 +12,17 @@ const Side = ({ children, content, ...rest }: SideProps) => {
     return (
         <div className="grid lg:grid-cols-2 h-full">
             <div
-                className="bg-no-repeat bg-cover py-6 px-16 flex-col justify-between hidden lg:flex"
+                className="bg-no-repeat bg-fixed bg-contain py-6 px-16 flex-col justify-between hidden lg:flex"
                 style={{
                     backgroundImage: `url('/img/others/auth-cover-bg.svg')`,
                 }}
             >
-                {/* <Logo mode="dark" type="full" /> */}
-                <img src="/img/logo/logo-dark-full.svg" alt="logo" />
-                {/* <div>
-                    <div className="mb-6 flex items-center gap-4">
-                        <Avatar
-                            className="border-2 border-white"
-                            shape="circle"
-                            src="/img/avatars/thumb-10.jpg"
-                        />
-                        <div className="text-white">
-                            <div className="font-semibold text-base">
-                                Brittany Hale
-                            </div>
-                            <span className="opacity-80">CTO, Onward</span>
-                        </div>
-                    </div>
-                    <p className="text-lg text-white opacity-80">
-                        Elstar comes with a complete set of UI components
-                        crafted with Tailwind CSS, it fulfilled most of the use
-                        case to create modern and beautiful UI and application
-                    </p>
-                </div> */}
-                {/* <span className="text-white">
-                    Copyright &copy; {`${new Date().getFullYear()}`}{' '}
-                    <span className="font-semibold">{`${APP_NAME}`}</span>{' '}
-                </span> */}
+                <div style={{ position: 'fixed', top: 32, left: 64, zIndex: 10 }}>
+                    <img src="/img/logo/logo-dark-full.svg" alt="logo" />
+                </div>
             </div>
             <div className="flex flex-col justify-center items-center bg-white dark:bg-gray-800">
-                <div className="w-full xl:max-w-[450px] px-8 max-w-[380px]">
+                <div className="w-full px-8">
                     <div className="mb-8">{content}</div>
                     {children
                         ? cloneElement(children as React.ReactElement, {
--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -2,12 +2,13 @@ import SignInForm from './SignInForm'
 
 const SignIn = () => {
     return (
-        <>
+        <div className="m-auto xl:max-w-[450px] max-w-[380px]">
             <div className="mb-8">
+                <h1 className="text-4xl font-bold">เข้าสู่ระบบ</h1>
                 <h3 className="mb-1">สำหรับผู้ประกอบการ</h3>
             </div>
             <SignInForm disableSubmit={false} />
-        </>
+        </div>
     )
 }
 
