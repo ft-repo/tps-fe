2de1f91df0ab039bf39eaa7e655aeefa87679c12
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-08-01 04:28:02 +0700

    fix: update constant

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,6 @@
-import { StatusColor } from "@/@types/shared";
+import { ApprovalStatusValue, StatusColor } from "@/@types/shared";
+
+export type ApprovalStatusKey = keyof typeof APPROVAL_STATUS;
 
 export const STATUS_COLOR: StatusColor = {
   "APPROVE": {
@@ -13,4 +15,23 @@ export const STATUS_COLOR: StatusColor = {
     "color": "#FFFFE0",
     "text": "กำลังดำเนินการ"
   }
+}
+
+export const APPROVAL_STATUS: Record<string, ApprovalStatusValue> = {
+  "APPROVED": {
+    "className": "bg-green-500 text-white border-0 rounded p-3",
+    "text": "ผ่านการตรวจ"
+  },
+  "IN_PROGRESS": {
+    "className": "bg-yellow-500 text-black border-0 rounded p-3",
+    "text": "รอดำเนินการ"
+  },
+  "WAIT_APPROVAL": {
+    "className": "bg-red-500 text-white border-0 rounded p-3",
+    "text": "รอตรวจสอบ"
+  },
+  "REJECTED": {
+    "className": "bg-gray-500 text-white border-0 rounded p-3",
+    "text": "ยุติ"
+  },
 }
