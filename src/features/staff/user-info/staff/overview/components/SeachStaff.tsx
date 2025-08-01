134ac0e0cae6bee18761f028ce4350c3f188a92b
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-08-01 15:26:03 +0700

    fix: applied search form to /user-info/staff

--- src/features/staff/user-info/staff/overview/components/SeachStaff.tsx
+++ src/features/staff/user-info/staff/overview/components/SeachStaff.tsx
@@ -1,25 +1,53 @@
 /* eslint-disable no-empty-pattern */
 /* eslint-disable react-refresh/only-export-components */
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Button, Input } from '@/components/ui'
+import { Controller, useForm } from 'react-hook-form';
+import { SearchUserName } from '@/@types/staff/user-info';
+
 
 interface Props { }
 
 const SeachStaff: React.FC<Props> = (props) => {
   const { } = props;
 
+  const form = useForm<SearchUserName>({
+    defaultValues: {
+      username: ''
+    }
+  })
+
+  const { control, handleSubmit } = form
+
+  const onSubmit = useCallback((value: SearchUserName) => {
+    console.log(value)
+  }, [])
+
+
   return (
-    <div className='flex items-center gap-3 xl:w-1/3'>
-      <Input
-        placeholder="ค้นหาชื่อผู้ใช้งาน"
-      />
-      <Button
-        type='submit'
-        variant='solid'
-      >
-        ค้นหา
-      </Button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='flex items-center gap-3 xl:w-1/3'>
+        <Controller
+          name='username'
+          control={control}
+          render={({ field }) => {
+            return (
+              <Input
+                {...field}
+                name={field.name}
+                placeholder="ค้นหาชื่อผู้ใช้งาน"
+              />
+            )
+          }}
+        />
+        <Button
+          type='submit'
+          variant='solid'
+        >
+          ค้นหา
+        </Button>
+      </div>
+    </form>
   )
 }
 
