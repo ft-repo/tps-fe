010fe7fb534e3e3df412f3e3e2c917b0869bc40a
Jessada Supapsuntorn <26509248+lzy3me@users.noreply.github.com>
2025-08-10 05:55:02 +0700

    feat: enhance SignUp form with contact information and file upload functionality

--- src/views/auth/SignUp/SignUp.tsx
+++ src/views/auth/SignUp/SignUp.tsx
@@ -3,93 +3,106 @@ import { useForm } from 'react-hook-form'
 import SignUpForm from './SignUpForm'
 import { SignUpCredential } from '@/@types/auth'
 import { useAppDispatch } from '@/store'
-import { getDistrict, getEntityType, getProvince, getSubDistrict } from '@/store/slices/master/masterSlice'
+import {
+  getDistrict,
+  getEntityType,
+  getProvince,
+  getSubDistrict,
+  getContactType,
+} from '@/store/slices/master/masterSlice'
 import { Button } from '@/components/ui'
 import useTimeOutMessage from '@/utils/hooks/useTimeOutMessage'
 import { ActionLink } from '@/components/shared'
 
 const SignUp = () => {
-    const dispatch = useAppDispatch()
-    const [message, setMessage] = useTimeOutMessage()
-    const [provinceId, setProvinceId] = useState<string | number>('')
-    const [districtId, setDistrictId] = useState<string | number>('')
-    const [isSubmitting, setIsSubmitting] = useState(false)
+  const dispatch = useAppDispatch()
+  const [message, setMessage] = useTimeOutMessage()
+  const [provinceId, setProvinceId] = useState<string | number>('')
+  const [districtId, setDistrictId] = useState<string | number>('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const form = useForm<SignUpCredential>({
-        defaultValues: {
-            password: '',
-            business_detail: {
-                business_name: '',
-                registration_no: '',
-                entity_type_id: 0,
-            },
-            business_address: {
-                house_number: '',
-                village: '',
-                lane: '',
-                road: '',
-                sub_district_id: 0,
-                district_id: 0,
-                province_id: 0,
-                zip_code: '',
-            },
-            business_document: {
-                certificate_file_url: '',
-                cid_card_file_url: '',
-                business_file_url: '',
-            },
-        },
-    })
+  const form = useForm<SignUpCredential>({
+    defaultValues: {
+      password: '',
+      business_detail: {
+        business_name: '',
+        registration_no: '',
+        entity_type_id: 0,
+      },
+      business_address: {
+        house_number: '',
+        village: '',
+        lane: '',
+        road: '',
+        sub_district_id: 0,
+        district_id: 0,
+        province_id: 0,
+        zip_code: '',
+      },
+      business_document: {
+        certificate_file_url: '',
+        cid_card_file_url: '',
+        business_file_url: '',
+      },
+      contact_info: {
+        contact_name: '',
+        contact_type_id: 0,
+        phone_number: '',
+        cid: '',
+      },
+    },
+  })
 
-    const { handleSubmit, control, setValue, getValues } = form
+  const { handleSubmit, control, setValue, getValues } = form
 
-    useEffect(() => {
-        dispatch(getProvince())
-        dispatch(getEntityType())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getProvince())
+    dispatch(getEntityType())
+    dispatch(getContactType())
+  }, [dispatch])
 
-    useEffect(() => {
-        if (provinceId) {
-            dispatch(getDistrict(provinceId.toString()))
-        }
-    }, [dispatch, provinceId])
+  useEffect(() => {
+    if (provinceId) {
+      dispatch(getDistrict(provinceId.toString()))
+    }
+  }, [dispatch, provinceId])
 
-    useEffect(() => {
-        if (districtId) {
-            dispatch(getSubDistrict(districtId.toString()))
-        }
-    }, [dispatch, districtId])
+  useEffect(() => {
+    if (districtId) {
+      dispatch(getSubDistrict(districtId.toString()))
+    }
+  }, [dispatch, districtId])
 
-    const onSubmit = useCallback(async (value: SignUpCredential) => {
-        console.log(value)
-    }, [])
+  const onSubmit = useCallback(async (value: SignUpCredential) => {
+    console.log(value)
+  }, [])
 
-    return (
-        <div className="m-auto xl:max-w-[600px] max-w-[450px]">
-            <div className="mb-8">
-                <h3 className="mb-1">ลงทะเบียนผู้ประกอบการ</h3>
-                <p>ลงทะเบียนผู้ประกอบการสำหรับการประเมินและขอใช้เส้นทาง</p>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <SignUpForm control={control} setValue={setValue} setProvinceId={setProvinceId} setDistrictId={setDistrictId} />
+  return (
+    <div className="m-auto xl:max-w-[600px] max-w-[450px]">
+      <div className="mb-8">
+        <h3 className="mb-1">ลงทะเบียนผู้ประกอบการ</h3>
+        <p>ลงทะเบียนผู้ประกอบการสำหรับการประเมินและขอใช้เส้นทาง</p>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <SignUpForm
+          control={control}
+          setValue={setValue}
+          setProvinceId={setProvinceId}
+          setDistrictId={setDistrictId}
+        />
 
-                <div className="mt-4">
-                    <Button
-                        block
-                        loading={isSubmitting}
-                        variant="solid"
-                        type="submit"
-                    >
-                        {isSubmitting ? 'กำลังสร้างบัญชี...' : 'ลงทะเบียน'}
-                    </Button>
-                </div>
-                <div className="mt-4 mb-8 text-center">
-                    <span>มีบัญชีอยู่แล้ว? </span>
-                    <ActionLink to={'/sign-in'}>เข้าสู่ระบบ</ActionLink>
-                </div>
-            </form>
+        <div className="mt-4">
+          <Button block loading={isSubmitting} variant="solid" type="submit">
+            {isSubmitting ? 'กำลังสร้างบัญชี...' : 'ลงทะเบียน'}
+          </Button>
         </div>
-    )
+        <div className="mt-4 mb-8 text-center">
+          <span>มีบัญชีอยู่แล้ว? </span>
+          <ActionLink to={'/sign-in'}>เข้าสู่ระบบ</ActionLink>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
--- src/views/auth/SignUp/SignUpForm.tsx
+++ src/views/auth/SignUp/SignUpForm.tsx
@@ -1,8 +1,15 @@
 import { SignUpCredential } from '@/@types/auth'
 import { Input } from '@/components/ui/Input'
 import { Select } from '@/components/ui/Select'
+import { Upload } from '@/components/ui/Upload'
+import {
+  postUploadFile,
+  postUploadImage,
+} from '@/services/entrepreneur/VehicleListService'
 import { useAppSelector } from '@/store'
+import { useCallback } from 'react'
 import { Control, Controller, UseFormSetValue } from 'react-hook-form'
+import { FaUpload as UploadIcon } from 'react-icons/fa6'
 
 interface Props {
   control: Control<SignUpCredential>
@@ -13,8 +20,34 @@ interface Props {
 
 function SignUpForm(props: Props) {
   const { control, setValue, setProvinceId, setDistrictId } = props
-  const { province, district, sub_district, entity_type } = useAppSelector(
-    (state) => state.master,
+  const { province, district, sub_district, entity_type, contact_type } =
+    useAppSelector((state) => state.master)
+
+  const uploadFile = useCallback(
+    async (fieldName: string, file: any, isImage: boolean = false) => {
+      let uploadAPI
+      if (isImage) {
+        uploadAPI = postUploadImage
+      } else {
+        uploadAPI = postUploadFile
+      }
+      try {
+        // POST
+        const response = await uploadAPI({ upload: file[0] })
+        if (response.status === 200) {
+          setValue([fieldName] as any, response.data?.url)
+        } else {
+          console.log('Error')
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message)
+        } else {
+          console.error(error)
+        }
+      }
+    },
+    [setValue],
   )
 
   return (
@@ -77,7 +110,13 @@ function SignUpForm(props: Props) {
             return (
               <fieldset className="col-span-2">
                 <label>เบอร์โทรสำนักงาน</label>
-                <Input {...field} name={field.name} placeholder="กรุณาระบุ" type="tel" autoComplete="off" />
+                <Input
+                  {...field}
+                  name={field.name}
+                  placeholder="กรุณาระบุ"
+                  type="tel"
+                  autoComplete="off"
+                />
               </fieldset>
             )
           }}
@@ -94,7 +133,11 @@ function SignUpForm(props: Props) {
                 return (
                   <fieldset>
                     <label>เลขที่</label>
-                    <Input {...field} name={field.name} placeholder="กรุณาระบุ" />
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                    />
                   </fieldset>
                 )
               }}
@@ -106,7 +149,11 @@ function SignUpForm(props: Props) {
                 return (
                   <fieldset>
                     <label>หมู่ที่</label>
-                    <Input {...field} name={field.name} placeholder="กรุณาระบุ" />
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                    />
                   </fieldset>
                 )
               }}
@@ -118,7 +165,11 @@ function SignUpForm(props: Props) {
                 return (
                   <fieldset>
                     <label>ซอย</label>
-                    <Input {...field} name={field.name} placeholder="กรุณาระบุ" />
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                    />
                   </fieldset>
                 )
               }}
@@ -130,7 +181,11 @@ function SignUpForm(props: Props) {
                 return (
                   <fieldset>
                     <label>ถนน</label>
-                    <Input {...field} name={field.name} placeholder="กรุณาระบุ" />
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                    />
                   </fieldset>
                 )
               }}
@@ -224,7 +279,183 @@ function SignUpForm(props: Props) {
                 return (
                   <fieldset>
                     <label>รหัสไปรษณีย์</label>
-                    <Input {...field} name={field.name} placeholder="กรุณาระบุ" disabled />
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                      disabled
+                    />
+                  </fieldset>
+                )
+              }}
+            />
+          </div>
+        </div>
+
+        {/* ผู้รับหมอบอำนาจ */}
+        <div className="mb-4 col-span-2">
+          <div className="font-semibold mb-2">ผู้ติดต่อ/รับหมอบอำนาจ</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Controller
+              name="contact_info.contact_name"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <fieldset>
+                    <label>ชื่อผู้ติดต่อ / มอบอำนาจ</label>
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                    />
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="contact_info.contact_type_id"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <fieldset>
+                    <label>ประเภทผู้ติดต่อ / มอบอำนาจ</label>
+                    <Select
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาเลือก"
+                      options={contact_type.map((item) => {
+                        return {
+                          label: item.name,
+                          value: item.id,
+                        }
+                      })}
+                      onChange={(e: any) => {
+                        setValue('contact_info.contact_type_id', e.value)
+                        field.onChange(e)
+                      }}
+                    />
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="contact_info.phone_number"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <fieldset>
+                    <label>เบอร์โทรผู้ติดต่อ</label>
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                      type="tel"
+                      autoComplete="off"
+                    />
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="contact_info.cid"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <fieldset>
+                    <label>หมายเลขบัตรประชาชน</label>
+                    <Input
+                      {...field}
+                      name={field.name}
+                      placeholder="กรุณาระบุ"
+                      type="text"
+                      autoComplete="off"
+                    />
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="business_document.business_file_url"
+              control={control}
+              rules={{
+                required: 'กรุณาอัปโหลดหนังสือรับรองนิติบุคคล',
+              }}
+              render={() => {
+                return (
+                  <fieldset className='col-span-2'>
+                    <label className="block">
+                      หนังสือรับรองนิติบุคคล (รองรับไฟล์ .pdf เท่านั้น)
+                    </label>
+                    <Upload
+                      draggable
+                      uploadLimit={1}
+                      onChange={(file) =>
+                        uploadFile('business_document.business_file_url', file)
+                      }
+                    >
+                      <div className=" text-center">
+                        <div className="text-xl flex justify-center">
+                          <UploadIcon />&nbsp;เพิ่มไฟล์
+                        </div>
+                      </div>
+                    </Upload>
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="business_document.cid_card_file_url"
+              control={control}
+              render={() => {
+                  return (
+                  <fieldset className='col-span-2'>
+                    <label className="block">รูปบัตรประชาชน (รองรับไฟล์ .pdf .png .jpeg .jpg)</label>
+                    <Upload
+                      draggable
+                      uploadLimit={1}
+                      onChange={(file) =>
+                        uploadFile('business_document.cid_card_file_url', file)
+                      }
+                    >
+                      <div className=" text-center">
+                        <div className="text-xl flex justify-center">
+                          <UploadIcon />&nbsp;เพิ่มไฟล์
+                        </div>
+                      </div>
+                    </Upload>
+                  </fieldset>
+                )
+              }}
+            />
+            <Controller
+              name="business_document.certificate_file_url"
+              control={control}
+              rules={{
+                required: 'กรุณาอัปโหลดรูปบริษัท / ผู้ติดต่อ / ผู้มอบอำนาจ',
+              }}
+              render={() => {
+                return (
+                  <fieldset className='col-span-2'>
+                    <label className="block">
+                      รูปบริษัท / ผู้ติดต่อ / ผู้มอบอำนาจ (รองรับไฟล์ .pdf .png
+                      .jpeg .jpg)
+                    </label>
+                    <Upload
+                      draggable
+                      uploadLimit={1}
+                      onChange={(file) =>
+                        uploadFile(
+                          'business_document.certificate_file_url',
+                          file,
+                        )
+                      }
+                    >
+                      <div className=" text-center">
+                        <div className="text-xl flex justify-center">
+                          <UploadIcon />&nbsp;เพิ่มไฟล์
+                        </div>
+                      </div>
+                    </Upload>
                   </fieldset>
                 )
               }}
