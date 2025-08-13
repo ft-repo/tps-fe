f1d81a84d2ed5e849e60f0ca7a84384ccc690976
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-08-13 23:03:45 +0700

    fix: update layout slice

--- src/store/slices/layout/LayoutSlice.ts
+++ src/store/slices/layout/LayoutSlice.ts
@@ -4,12 +4,13 @@ import { SLICE_BASE_NAME } from './constants';
 export type LayoutState = {
   loading: boolean;
   fullscreen_loading: boolean;
+  open_modal: boolean;
 }
 
 const initialState: LayoutState = {
   loading: false,
-  fullscreen_loading: false
- 
+  fullscreen_loading: false,
+  open_modal: false
 }
 
 // export const SLICE_NAME = 'layoutSlice';
@@ -18,16 +19,18 @@ const layoutSlice = createSlice({
   name: SLICE_BASE_NAME,
   initialState,
   reducers: {
-      
     setLoading: (state, action) => {
       state.loading = action.payload
     },
     setFullscreenLoading: (state, action) => {
       state.fullscreen_loading = action.payload
+    },
+    setOpenModal: (state, action) => {
+      state.open_modal = action.payload
     }
   }
 })
 
-export const { setLoading, setFullscreenLoading } = layoutSlice.actions
+export const { setLoading, setFullscreenLoading, setOpenModal } = layoutSlice.actions
 
 export default layoutSlice.reducer
