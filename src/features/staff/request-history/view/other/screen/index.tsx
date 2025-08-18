c12c846e38bf5ea4c0a01e268dafaf142c2fe1b2
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-08-18 22:06:15 +0700

    fix: update history structure

--- src/features/staff/request-history/view/other/screen/index.tsx
+++ src/features/staff/request-history/view/other/screen/index.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable no-empty-pattern */
 /* eslint-disable react-refresh/only-export-components */
 import React from 'react'
+import { Button } from 'antd';
+import { ContentSection } from '../components';
+import { AiOutlineLeft } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
 
@@ -8,9 +12,23 @@ interface Props {
 
 const OtherScreen: React.FC<Props> = (props) => {
   const { } = props
+  const navigate = useNavigate()
 
   return (
-    <div>index</div>
+    <>
+      <section>
+        <Button
+          type='text'
+          icon={<AiOutlineLeft />}
+          onClick={() => navigate(-1)}
+        >
+          ย้อนกลับ
+        </Button>
+      </section>
+      <section className='mt-5'>
+        <ContentSection />
+      </section>
+    </>
   )
 }
 
