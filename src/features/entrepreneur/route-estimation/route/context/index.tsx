724f6d818082312e228ef522878ceab24c9a984d
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-08-27 19:27:09 +0700

    fix: update data parser

--- src/features/entrepreneur/route-estimation/route/context/index.tsx
+++ src/features/entrepreneur/route-estimation/route/context/index.tsx
@@ -1,28 +1,51 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-refresh/only-export-components */
-import { ContextProps, RouteEstimationRequest } from '@/@types/entrepreneur/route-estimation'
-import React, { createContext, useContext, useState } from 'react'
+import { PetitionEstimateRequest, PetitionEstimateResponse } from '@/@types/services/petition';
+import { createContext, useContext, useState } from 'react'
+
+export interface ContextProps {
+  step: number;
+  setStep: (step: number | any) => void;
+  dataParser: DataParser;
+  setDataParser: (dataParser: DataParser) => void;
+}
+
+export interface DataParser extends MatchType {
+  req_data: PetitionEstimateRequest;
+  res_data: PetitionEstimateResponse;
+}
+
+export interface MatchType {
+  match_type: number;
+}
 
 export const PageContext = createContext<ContextProps | null>(null)
 
 export const RouteProvider = (props: any) => {
   const { children } = props
   const [step, setStep] = useState<number>(1)
-  const [dataParser, setDataParser] = useState<RouteEstimationRequest>({
-    vehicle: [],
-    start_point: {
-      type: 'Point',
-      coordinates: [0, 0],
+  const [dataParser, setDataParser] = useState<DataParser>({
+    req_data: {
+      vehicle: [],
+      start_point: {
+        type: 'Point',
+        coordinates: [0, 0],
+      },
+      end_point: {
+        type: 'Point',
+        coordinates: [0, 0],
+      },
+      vehicle_route: {
+        type: 'LineString',
+        coordinates: [],
+      },
     },
-    end_point: {
-      type: 'Point',
-      coordinates: [0, 0],
+    res_data: {
+      estimate: [],
+      set_id: ''
     },
-    vehicle_route: {
-      type: 'LineString',
-      coordinates: [],
-    },
+    match_type: 0
   })
 
   return (
