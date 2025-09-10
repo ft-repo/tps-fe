dec6f23e1b7e8d6dac8c3916a0a0abda3aea770f
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-09-11 05:05:25 +0700

    feat: added context

---
+++ src/features/staff/request-list/approval/route/context/index.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-refresh/only-export-components */
+import { EstimateRouteSubDetail } from '@/@types/reducer/petition';
+import { createContext, useContext, useState } from 'react'
+
+export interface ContextProps {
+  index: number;
+  item: EstimateRouteSubDetail;
+  setIndex: (value: number) => void;
+  setItem: (value: EstimateRouteSubDetail) => void;
+}
+
+export const PageContext = createContext<ContextProps | null>(null)
+
+export const RouteProvider = (props: any) => {
+  const { children } = props
+  const [index, setIndex] = useState<number>(0)
+  const [item, setItem] = useState<EstimateRouteSubDetail>({
+    estimate_id: '',
+    sort: ''
+  })
+
+  return (
+    <PageContext.Provider
+      value={{
+        index,
+        item,
+        setIndex,
+        setItem
+      }}
+    >
+      {children}
+    </PageContext.Provider>
+  )
+}
+
+export const useRouteContext = () => {
+  const context = useContext(PageContext);
+  if (!context) {
+    throw new Error("useRouteContext must be used within an RouteProvider");
+  }
+  return context;
+};
