8247ed13a8c8e35f131734e56f23b73322f14297
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2025-09-15 15:05:22 +0700

    fix: update where tabItem bug when go back to prev step

--- src/features/entrepreneur/route-estimation/route/components/route-estimate/initial/ContentTab.tsx
+++ src/features/entrepreneur/route-estimation/route/components/route-estimate/initial/ContentTab.tsx
@@ -5,6 +5,7 @@ import { Tabs, type TabsProps } from 'antd'
 import { Control, useFieldArray, UseFormSetValue } from 'react-hook-form'
 import { FieldTypeArr } from '@/@types/entrepreneur/route-estimation'
 import FormVehicle from './FormVehicle';
+import { useRouteContext } from '../../../context';
 
 interface Props {
   control: Control<FieldTypeArr>;
@@ -17,29 +18,66 @@ type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
 const ContentTab: React.FC<Props> = (props) => {
   const { control, setValue } = props
   const { fields, append, remove } = useFieldArray({ control, name: 'route_form' })
+  const { dataParser } = useRouteContext()
   // DECLARE INIT STATE
-  const initTabItems: TabItem[] = [
-    {
-      key: '1',
-      label: 'รถคู่ที่ 1',
-      children: (
-        <FormVehicle
-          formItem={fields[0]}
-          formIndex={0}
-          control={control}
-          setValue={setValue}
-        />
-      ),
-      closable: false,
-    },
-  ]
+  const initTabItems: TabItem[] = dataParser.req_data.vehicle.length ?
+    dataParser.req_data.vehicle.map((item, index) => {
+      return {
+        key: String(index),
+        label: `รถคู่ที่ ${index + 1}`,
+        children: (
+          <FormVehicle
+            formItem={fields[index]}
+            formIndex={index}
+            control={control}
+            setValue={setValue}
+          />
+        ),
+        closable: index === 0 ? false : true,
+      }
+    })
+    : [
+      {
+        key: '1',
+        label: 'รถคู่ที่ 1',
+        children: (
+          <FormVehicle
+            formItem={fields[0]}
+            formIndex={0}
+            control={control}
+            setValue={setValue}
+          />
+        ),
+        closable: false,
+      },
+    ]
   // DECLARE STATE
   const [tabKey, setTabKey] = useState(initTabItems[0].key)
   const [tabItems, setTabItems] = useState<TabItem[]>(initTabItems)
 
   const onAdd = useCallback(() => {
     // APEND FIELD
-    append(fields[0])
+    append({
+      "match_type": null,
+      "turn_radius": "",
+      "towering_vehicle": null,
+      "semi_trailer_vehicle": null,
+      "etc_vehicle": null,
+      "towering_weight1": 0,
+      "towering_weight2": 0,
+      "towering_weight3": 0,
+      "towering_weight4": 0,
+      "towering_weight5": 0,
+      "towering_weight6": 0,
+      "towering_weight7": 0,
+      "semi_weight1": 0,
+      "semi_weight2": 0,
+      "semi_weight3": 0,
+      "semi_weight4": 0,
+      "semi_weight5": 0,
+      "semi_weight6": 0,
+      "semi_weight7": 0,
+    })
     // APPEND TAB
     const newTabItem: TabsProps['items'] = [
       {
@@ -79,15 +117,15 @@ const ContentTab: React.FC<Props> = (props) => {
 
   const onEdit = useCallback((targetKey: React.MouseEvent | React.KeyboardEvent | string, action: 'add' | 'remove') => {
     if (action === 'add') {
-      const MAX_TABS = 1;
-      if (tabItems.length >= MAX_TABS) {
-        return; // Don't add if limit reached
-      }
+      // const MAX_TABS = 1;
+      // if (tabItems.length >= MAX_TABS) {
+      //   return; // Don't add if limit reached
+      // }
       onAdd()
     } else {
       onRemove(targetKey)
     }
-  }, [onAdd, onRemove, tabItems])
+  }, [onAdd, onRemove])
 
   const onChange = useCallback((newActiveKey: string) => {
     setTabKey(newActiveKey)
