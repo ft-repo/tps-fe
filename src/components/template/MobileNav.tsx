5c0e299ea30367e9e08ff217b3ea3234c7dd3f96
Klanarm <klanarong_r@hotmail.com>
2026-08-27 16:38:42 +0700

    fix: update redirect URL in MobileNav component

--- src/components/template/MobileNav.tsx
+++ src/components/template/MobileNav.tsx
@@ -160,7 +160,7 @@ const MobileNav = () => {
 				}}
 				onClick={() => {
 					if (authority[0] === 'USER' && from_web === false) {
-						window.location.href = `https://eservice.drr.go.th/uat/dga/back_to_web?mToken=${m_token}`
+						window.location.href = `https://eservice.drr.go.th/dga/back_to_web?mToken=${m_token}`
 					} else {
 						signOut()
 					}
